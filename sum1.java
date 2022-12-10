6ee685358f6f822b7400cbeb63a229ce8feaf861
s367-rgukt <120241620+s367-rgukt@users.noreply.github.com>
2022-12-10 13:08:51 +0530

    Add files via upload

---
+++ sum1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class sum1
+{
+public static void main(String args[])
+{
+int i, sum=0;
+for(i=1;i<=100;i++)
+{
+sum=sum+i;
+}
+System.out.println("sum of first 100 natural numbers is:"+sum);
+}
+}
